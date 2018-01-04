c141f6d1b25be2d7bb2bb48b3f9e3fa175f973c2
Johannes Brännström <jbrannst@redhat.com>
2018-01-04 16:16:12 +0100

    modify for minishift

--- dynamicvdb-datafederation/jdv-ocp-setup.sh
+++ dynamicvdb-datafederation/jdv-ocp-setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 echo 'Logging into oc tool as admin'
-oc login https://10.1.2.2:8443 -u admin -p admin
+oc login -u admin -p admin
 echo 'Switching to the openshift project'
 oc project openshift
 echo 'Creating the image stream for the OpenShift datavirt image'
+oc delete is jboss-datavirt63-openshift
 oc create -f https://raw.githubusercontent.com/cvanball/jdv-ose-demo/master/extensions/is.json
 echo 'Creating the s2i quickstart template. This will live in the openshift namespace and be available to all projects'
+oc delete template datavirt63-ext-mysql-psql-s2i
 oc create -n openshift -f https://raw.githubusercontent.com/cvanball/jdv-ose-demos/master/templates/datavirt63-ext-mysql-psql-s2i.json
-oc login -u openshift-dev -p devel
+oc login -u developer -p developer
 echo 'Creating a new project called jdv-datafederation-demo'
 oc new-project jdv-datafederation-demo
 echo 'Creating a service account and accompanying secret for use by the datavirt application'
@@ -23,4 +25,4 @@ oc new-app --template=datavirt63-ext-mysql-psql-s2i
 echo '==============================================='
 echo 'The following urls will allow you to access the Financials VDB via OData4:'
 echo '==============================================='
-echo 'http://datavirt-app-jdv-datafederation-demo.rhel-cdk.10.1.2.2.xip.io/odata4/Financials/All_Customers/CUSTOMER?$format=json'
+echo "http://datavirt-app-jdv-datafederation-demo.$(minishift ip).nip.io/odata4/Financials/All_Customers/CUSTOMER?$format=json'
